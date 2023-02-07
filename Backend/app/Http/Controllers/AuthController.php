43ddf7649a624aed2f413150aa597d7afc4b8e4d
vanzar-codigo <vanzarlyanhtan@supers.team>
2023-02-07 21:16:30 +0630

    working: on database

---
+++ Backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
+
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = JWTAuth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = JWTAuth::user();
+        return response()->json([
+                'status' => 'success',
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+
+    }
+
+    public function register(Request $request){
+    
+        $validator = validator($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',]);
+        
+            if($validator->fails()) {
+                // return as appropriate
+                return response()->json([
+                    'message' => 'Your email has been already registered',
+                    'errorCode' => 422,
+                ], 200);
+                }
+
+        $credentials = $request->only('email', 'password');
+        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+        ]);
+        $token = JWTAuth::attempt($credentials);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    public function logout()
+    {
+        JWTAuth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => JWTAuth::user(),
+            'authorisation' => [
+                'token' => JWTAuth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+}
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\dummyAPI;
-
-Route::apiResource('users', UserController::class);
+use App\Http\Controllers\AuthController;
+use Routes\config\auth;
 
 Route::get('data', [dummyAPI::class, 'getData']);
-
+Route::apiResource('users', UserController::class);
 Route::get('users', [dummyAPI::class, 'getUsers']);
+
+// Authentication
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('users', [UserController::class, 'index']);
+    Route::get('dummy', [dummyAPI::class, 'getUsers']);
+});
