12d832e33fa3ada2a7b3da4ac4b50435226aefa4
maythukywe <maythukywe9@gmail.com>
2023-03-06 21:02:52 +0630

    IdeaAPIDone

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use Routes\config\auth;
 use App\Http\Controllers\DepartmentsController;
+use App\Http\Controllers\IdeasController;
 
 Route::get('data', [dummyAPI::class, 'getData']);
 Route::apiResource('users', UserController::class);
@@ -32,4 +33,13 @@ Route::group(['middleware' => 'auth.jwt'], function () {
     Route::get('category_lists', [CategoryController::class, 'index']);
     Route::post('category_add', [CategoryController::class, 'store']);
     Route::post('category_delete/{id}', [CategoryController::class, 'destroy']);
+
+    //Idea
+    Route::get('ideas', [IdeasController::class, 'index']);
+    Route::get('ideas/{id}', [IdeasController::class, 'index']);
+    Route::post('ideas/add', [IdeasController::class, 'store']);
+    Route::post('ideas/update/{id}', [IdeasController::class, 'update']);
+
 });
+
+
