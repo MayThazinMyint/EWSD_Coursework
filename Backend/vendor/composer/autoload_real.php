49753b6c2f72b84c780b4fd357bc996b604b09ae
MayThazin30 <maythazinmyint@acedatasystems.com>
2023-03-25 21:26:33 +0630

    compose update

--- Backend/vendor/composer/autoload_real.php
+++ Backend/vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInit033a5ec0e427b241d799da2f6f8a7c5a
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit033a5ec0e427b241d799da2f6f8a7c5a::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        };
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInit033a5ec0e427b241d799da2f6f8a7c5a::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire033a5ec0e427b241d799da2f6f8a7c5a($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire033a5ec0e427b241d799da2f6f8a7c5a($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
